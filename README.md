e1e6bb35274433833665078cc4e5cbd361cea6bc
BugHunter1234 <444811716@qq.com>
2016-11-01 16:57:01 +0800

    修改一些BUG，增加一些注释

--- README.md
+++ README.md
@@ -4,22 +4,35 @@ HTML5触摸事件(touchstart、touchmove和touchend) HBuilder，MUI
 触摸事件(touch)会在用户手指放在屏幕上面的时候、在屏幕上滑动的时候或者是从屏幕上移开的时候出发。下面具体说明：
 
 　　touchstart事件：当手指触摸屏幕时候触发，即使已经有一个手指放在屏幕上也会触发。
+
 　　touchmove事件：当手指在屏幕上滑动的时候连续地触发。在这个事件发生期间，调用preventDefault()事件可以阻止滚动。
+
 　　touchend事件：当手指从屏幕上离开的时候触发。
+
 　　touchcancel事件：当系统停止跟踪触摸的时候触发。关于这个事件的确切出发时间，文档中并没有具体说明，咱们只能去猜测了。
-  
+
 　　上面的这些事件都会冒泡，也都可以取消。虽然这些触摸事件没有在DOM规范中定义，但是它们却是以兼容DOM的方式实现的。所以，每个触摸事件的event对象都提供了在鼠标实践中常见的属性：bubbles(起泡事件的类型)、cancelable(是否用 preventDefault() 方法可以取消与事件关联的默认动作)、clientX(返回当事件被触发时，鼠标指针的水平坐标)、clientY(返回当事件触发时，鼠标指针的垂直坐标)、screenX(当某个事件被触发时，鼠标指针的水平坐标)和screenY(返回当某个事件被触发时，鼠标指针的垂直坐标)。除了常见的DOM属性，触摸事件还包含下面三个用于跟踪触摸的属性。
 
 　　touches：表示当前跟踪的触摸操作的touch对象的数组。
+
 　　targetTouches：特定于事件目标的Touch对象的数组。
+
 　　changeTouches：表示自上次触摸以来发生了什么改变的Touch对象的数组。
 
 　　每个Touch对象包含的属性如下。
+
 　　clientX：触摸目标在视口中的x坐标。
+
 　　clientY：触摸目标在视口中的y坐标。
+
 　　identifier：标识触摸的唯一ID。
+
 　　pageX：触摸目标在页面中的x坐标。
+
 　　pageY：触摸目标在页面中的y坐标。
+
 　　screenX：触摸目标在屏幕中的x坐标。
+
 　　screenY：触摸目标在屏幕中的y坐标。
+
 　　target：触目的DOM节点目标。
--- js/TouchMove.js
+++ js/TouchMove.js
@@ -1,7 +1,7 @@
 /**
  *作者：444811716@qq.com
  *时间：2016-10-28
- *描述：手势移动事件
+ *描述：手势移动事件，可以移动，放大，缩小元素(该元素的position必须是绝对布局absolute)
  */
 var TouchMove = (function(mod) {
 
@@ -27,7 +27,7 @@ var TouchMove = (function(mod) {
 	//screenY：触摸目标在屏幕中的y坐标。
 	//target：触目的DOM节点目标。
 
-	//获取页面中的宽高
+	//记录页面范围
 	var screen = {
 		width: 0,
 		height: 0
@@ -37,7 +37,7 @@ var TouchMove = (function(mod) {
 	//移动table时记录手指的坐标
 	var movedx = 0;
 	var movedy = 0;
-	//放大缩小table时记录手指坐标
+	//放大缩小table时记录手指坐标的变化
 	var changex = 0;
 	var changey = 0;
 
@@ -49,24 +49,26 @@ var TouchMove = (function(mod) {
 		height: 0 //高
 	}
 
-	//记录doubletap的坐标
+	//记录双击时table的坐标
 	var doubleTap_x = 0;
 	var doubleTap_y = 0;
 
 	/**
 	 * 增加监听
+	 * @param {Object} elment 移动，放大的元素
 	 */
 	mod.addTouchListener = function(elment) {
+		//获取屏幕范围
 		screen = {
 			height: plus.screen.resolutionHeight - plus.navigator.getStatusbarHeight(), //屏幕高度-状态栏高度
 			width: plus.screen.resolutionWidth //屏幕宽度
 		};
+		//获得该元素
 		table = elment;
 
 		//获取table的宽高
 		var table_width = table.offsetWidth;
 		var table_height = table.offsetHeight;
-
 		//根据宽高的大小判断是否放大和如何放大
 		if(table_width > table_height) {
 			//宽大于高
@@ -85,19 +87,15 @@ var TouchMove = (function(mod) {
 		}
 
 		//设置table居中
-		//获取设置后table的宽高
-		var table_width = table.offsetWidth;
-		var table_height = table.offsetHeight;
-
 		table.style.left = (screen.width - table_width) / 2 + 'px';
 		table.style.top = (screen.height - table_height) / 2 + 'px';
 
-		//增加监听
-		//		document.addEventListener('touchstart', touch);
+		//页面增加Touch的监听
+		//document.addEventListener('touchstart', touch);
 		document.addEventListener('touchmove', touch);
 		document.addEventListener('touchend', touch);
 
-		//双击table
+		//增加双击table的监听
 		table.addEventListener('doubletap', doubleTap);
 	}
 
@@ -146,32 +144,33 @@ var TouchMove = (function(mod) {
 	}
 
 	/**
-	 * movetouch回调
+	 * Touch回调
 	 * @param {Object} event 触碰对象
 	 */
 	function touch(event) {
 		var event = event || window.event;
 		switch(event.type) {
-			//			case "touchstart": //开始触碰
-			//				onsole.log("touchstart");
-			//				break;
+			//case "touchstart": //开始触碰
+			//onsole.log("touchstart");
+			//break;
 			case "touchend": //结束触碰
 				//console.log("touchend");
-				//移动后没有触点重置记录移动的坐标
+				//结束触碰后重置记录移动的坐标
 				movedx = 0;
 				movedy = 0;
-				//记录双击屏幕后的坐标
+				//结束触碰后记录双击后触点的坐标
 				doubleTap_x = parseInt(event.changedTouches[0].clientX);
 				doubleTap_y = parseInt(event.changedTouches[0].clientY);
 				break;
 			case "touchmove": //开始移动
 				//console.log("touchmove");
-				event.preventDefault(); //阻止滚动
+				event.preventDefault(); //阻止页面滚动
 				if(event.touches[1] != undefined) {
 					//有第二个触点
-					changeTable(table, event.touches[0], event.touches[1]); //放大缩小table
+					changeTable(event.touches[0], event.touches[1]); //放大缩小table
 				} else {
-					moveTable(event.touches[0], table); //移动table
+					//只有一个触点
+					moveTable(event.touches[0]); //移动table
 				}
 				break;
 			default:
@@ -181,24 +180,23 @@ var TouchMove = (function(mod) {
 
 	/**
 	 * 移动元素
-	 * @param {Object} data 点击对象的数据
-	 * @param {Object} table 点击对象
+	 * @param {Object} data 元素的数据
+	 * @param {Object} table 元素
 	 */
-	function moveTable(data, table) {
+	function moveTable(data) {
 		if(data.target.getAttribute('id') == table.id) {
 			if(movedx == 0) {
 				movedx = data.clientX;
 				movedy = data.clientY;
 			} else {
-				//判断移动方向
+				//判断移动方向和距离
 				var x = movedx - data.clientX;
 				var y = movedy - data.clientY;
-
+				//获取table坐标和宽高
 				var table_x = table.offsetLeft;
 				var table_y = table.offsetTop;
 				var table_width = table.offsetWidth;
 				var table_height = table.offsetHeight;
-
 				if(x >= 0) { //左移
 					table.style.left = (table_x - Math.abs(x)) + 'px';
 				} else { //右移
@@ -220,7 +218,6 @@ var TouchMove = (function(mod) {
 				var table_y = table.offsetTop;
 				var table_width = table.offsetWidth;
 				var table_height = table.offsetHeight;
-
 				//限制右移区域
 				if(table_x >= screen.width / 4 && x < 0) {
 					//如果x坐标大于屏幕宽度的1/4，并且右移时
@@ -257,17 +254,18 @@ var TouchMove = (function(mod) {
 
 	/**
 	 * 改变table的大小
-	 * @param {Object} table 放大的对象
 	 * @param {Object} touch1 触点
 	 * @param {Object} touch2 触点2
 	 */
-	function changeTable(table, touch1, touch2) {
+	function changeTable(touch1, touch2) {
+		//parseInt()获取整数
 		//touch1坐标
 		var x1 = parseInt(touch1.clientX);
 		var y1 = parseInt(touch1.clientY);
 		//touch2坐标
 		var x2 = parseInt(touch2.clientX);
 		var y2 = parseInt(touch2.clientY);
+		//Math.abs()获取绝对值
 		//获取touch之间的距离
 		var x = Math.abs(x1 - x2);
 		var y = Math.abs(y1 - y2);
